5d34bf23c83e127dfefe415999ce17fc7fb51673
sandymosaad <sandyzakher5@gmail.com>
2025-08-14 09:18:09 +0300

    tasks011-018 part 2

--- tasks011-018.py
+++ tasks011-018.py
@@ -122,3 +122,90 @@ print(num4.zfill(4))
 print(num5.zfill(4))
 
 #--------------------------------------------------------------------------------
+# Problem 07:
+# Add @ symbols before any string you are given, so that the total length does not exceed 20 characters.
+
+# Example:
+name_one = "Osama"
+name_two = "Osama_Elzero"
+
+# Needed Output
+# @@@@@@@@@@@@@@@Osama
+# @@@@@@@@Osama_Elzero
+
+print(name_one.rjust(20, '@'))
+print(name_two.rjust(20, '@'))
+#--------------------------------------------------------------------------------
+# Problem 08:
+# Given the variables name_one and name_two, swap the case of each letter:
+
+# Convert uppercase letters to lowercase
+
+# Convert lowercase letters to uppercase
+
+# Example:
+
+name_one = "OSamA"
+name_two = "osaMA"
+
+# Needed Output
+# osAMa
+# OSAma
+
+print(name_one.swapcase())
+print(name_two.swapcase())
+#--------------------------------------------------------------------------------
+# Problem 09:
+# Given the string msg, count how many times the word "Love" appears in it.
+
+# Example:
+msg = "I Love Python And Although Love Elzero Web School"
+
+# Needed Output
+# 2
+
+print(msg.count("Love"))
+#--------------------------------------------------------------------------------
+# Problem 10:
+# Given the string name = "Elzero", print the position (index) of the letter z in the word.
+
+# Example:
+# name = "Elzero"
+
+# Needed Output
+# 2
+
+print(name.index("z"))
+#--------------------------------------------------------------------------------
+# Problem 11:
+# Given the string msg = "I <3 Python And Although <3 Elzero Web School", replace the first occurrence of the substring "<3" with the word "Love" only once.
+
+# Example:
+# msg = "I <3 Python And Although <3 Elzero Web School"
+
+# Needed Output
+# I Love Python And Although <3 Elzero Web School
+
+print(msg.replace("<3", "Love", 1))
+#--------------------------------------------------------------------------------
+# Problem 12:
+#Given the string msg = "I <3 Python And Although <3 Elzero Web School", replace all occurrences of the substring "<3" with the word "Love".
+# Example:
+# msg = "I <3 Python And Although <3 Elzero Web School"
+
+# Needed Output
+# I Love Python And Although 3< Elzero Web School
+
+print(msg.replace("<3", "Love"))
+#--------------------------------------------------------------------------------
+# Problem 13:
+# Create three variables containing your name, age, and country. Make sure that age is an integer, not a string. Then, print the following message using the new f-string formatting method (f""):
+
+name = "Osama"
+age = 38
+country = "Egypt"
+
+# Needed Output Using f""
+# My Name Is Osama, And My Age Is 38, And My Country Is Egypt
+
+print(f"My name is {name}, and my age is {age}, and my country is {country}")
