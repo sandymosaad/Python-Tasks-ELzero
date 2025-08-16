e29596c66985e8a4758fa40cff2f5ce073ad6377
sandymosaad <sandyzakher5@gmail.com>
2025-08-16 17:28:20 +0300

    tasks019-020 Problem 2

--- tasks019-020.py
+++ tasks019-020.py
@@ -12,5 +12,28 @@ print(type(10.5))
 # Complex
 print(1 + 2j)
 print(type(1+ 2j))
+
+print ("-----------------task 1 done ---------------")
 #--------------------------------------------------------
+
+# Problem 02:
+# Given the complex number 1+2j, print its imaginary part on the first line and its real part on the second line.
+
+# Example:
+
+num = 1 + 2j
+
+# Print Imaginary Part Here
+# Print Real Part Here
+
+# Needed Output:
+
+# 2.0
+# 1.0
+print(num.imag)
+print(num.real)
+
+print ("-----------------task 2 done ---------------")
+#--------------------------------------------------------------
+
 
