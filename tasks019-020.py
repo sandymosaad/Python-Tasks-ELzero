41c902e8afb00d4b59fc66f4110cdf23dac074f1
sandymosaad <sandyzakher5@gmail.com>
2025-08-16 17:24:31 +0300

    tasks019-020 Problem 1

---
+++ tasks019-020.py
@@ -0,0 +1,16 @@
+#Problem1:
+#Print all the main types of numbers in Python, each on a separate line.
+
+# Integer
+print(10)
+print(type(10))
+
+# Float
+print(10.5)
+print(type(10.5))
+
+# Complex
+print(1 + 2j)
+print(type(1+ 2j))
+#--------------------------------------------------------
+
