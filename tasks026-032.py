d00a7c235084799c408269326a0489ab393d6808
sandymosaad <sandyzakher5@gmail.com>
2025-09-06 11:02:02 +0300

    tasks026-032 Problem 02

--- tasks026-032.py
+++ tasks026-032.py
@@ -16,3 +16,35 @@ print(type(my_list))
 
 my_list.pop()
 print(my_list)
+print("--------------------task 1 done -----------------------")
+#---------------------------------------------------------------
+# Problem 02:
+# Create a set containing numbers from 1 to 3.
+# Create another set containing letters from "A" to "C".
+# Concatenate them into a new set.
+# Print the new set.
+# Print the new set again using a different method.
+nums = {1, 2, 3}
+letters = {"A", "B", "C"}
+
+# Needed Output
+# {1, 2, 3, "A", "B", "C"}
+# {1, 2, 3, "A", "B", "C"}
+# {1, 2, 3, "A", "B", "C"}
+
+# Concatenate sets in three different ways
+
+# 1. use union
+nums_and_letters1 = nums.union(letters)
+print(nums_and_letters1)
+
+# 2. use update (modifies nums itself)
+nums2 = {1, 2, 3}  # redefine nums so it doesn't change for the following examples
+nums2.update(letters)
+print(nums2)
+
+# 3. use | (or)
+nums_and_letters3 = nums | letters
+print(nums_and_letters3)
+print("--------------------task 2 done -----------------------")
+#---------------------------------------------------------------
