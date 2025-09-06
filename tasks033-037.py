182031f8a33ad2b40e9cb58eab34e4e4ecc00795
sandymosaad <sandyzakher5@gmail.com>
2025-09-06 11:28:58 +0300

    tasks033-037 Problem 01

---
+++ tasks033-037.py
@@ -0,0 +1,28 @@
+# Problem 01:
+
+# in the first 4 lines, using Bool Method, print 4 different data types that result in True
+# in lines 5 to 8, using Bool Method, print 4 different data types that result in False
+
+# Needed Output
+# True
+# True
+# True
+# True
+# False
+# False
+# False
+# False
+
+print(bool(1))
+print(bool("Hello"))
+print(bool([1, 2, 3]))
+print(bool({1, 2, 3}))  
+print(bool(0))
+print(bool("")) 
+print(bool([]))
+print(bool({}))
+print(bool(None))
+
+print("--------------------task 1 done -----------------------")
+#---------------------------------------------------------------
+
