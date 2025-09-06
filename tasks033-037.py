cc7b3a6d77a6115a8dbc192f5823175a3ce4221e
sandymosaad <sandyzakher5@gmail.com>
2025-09-06 11:32:19 +0300

    tasks033-037 Problem 02

--- tasks033-037.py
+++ tasks033-037.py
@@ -25,4 +25,16 @@ print(bool(None))
 
 print("--------------------task 1 done -----------------------")
 #---------------------------------------------------------------
+# Problem 02:
+# create three variables with names of any programming skills you have, and their values should be your proficiency percentage in those skills, all above 50%.
+# In a single line, using Boolean Operators, check if your proficiency in all skills is greater
+# than 50%. Do not use If Condition here, and the result should be True.
+html = 80
+css = 60
+javascript = 70
 
+# Needed Output
+#True
+print(html > 50 and css > 50 and javascript > 50)
+print("--------------------task 2 done -----------------------")
+#---------------------------------------------------------------
