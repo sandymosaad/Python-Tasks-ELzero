05ea9ec18efb11c92a3b0d67a6c6609575134073
sandymosaad <sandyzakher5@gmail.com>
2025-09-06 10:23:56 +0300

    tasks024-025 Problem 01

---
+++ tasks024-025 .py
@@ -0,0 +1,19 @@
+# Problem 01:
+# Create a Tuple with only one element (your name) without using parentheses ().
+
+# On the first line, print the single element in the tuple.
+
+# On the second line, print the type to make sure it is a tuple.
+
+# Needed Output
+
+# Osama
+# <class 'tuple'>
+
+
+name_tuple = "Osama",
+print(name_tuple[0])
+print(type(name_tuple))
+
+print("--------------------task 1 done -----------------------")
+#---------------------------------------------------------------
