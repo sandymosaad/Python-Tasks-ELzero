a7d1aebfd6b5ba1db62ac8c7e2d24f9a6aec0b1b
sandymosaad <sandyzakher5@gmail.com>
2025-09-08 19:16:21 +0300

    tasks038-040 Problem 01

---
+++ tasks038-040.py
@@ -0,0 +1,16 @@
+# Problem 01:
+
+# Create a variable named name that takes the user's input for their name.
+# Make sure to remove any spaces before and after the name.
+# Make sure the name is capitalized (first letter capital, rest small).
+# Print the name with a welcome message.
+# Input
+#"     osAmA   "
+
+# Needed Output
+#"Hello Osama, Happy To See You Here."
+
+input_Name = input("Enter Your Name: ").strip().capitalize()
+print(f"Hello {input_Name}, Happy to see you here")
+print("--------------------task 1 done -----------------------")
+#---------------------------------------------------------------
