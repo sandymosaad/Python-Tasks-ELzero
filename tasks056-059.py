9438b7aa8927f1d7a35126c0c316a16cbc5e034e
sandymosaad <sandyzakher5@gmail.com>
2025-09-22 21:35:25 +0300

    tasks056-059 Problem 02

--- tasks056-059.py
+++ tasks056-059.py
@@ -32,3 +32,32 @@ print(calculate(10, 20, "ADD"))  # 30
 print(calculate(10, 20, "s"))    # -10
 print(calculate(10, 20, "M"))    # 200
 print(calculate(10, 20, "x"))    # Invalid Operator
+
+print("--------------------task 1 done -----------------------")
+#---------------------------------------------------------------
+"""
+Problem 2:
+Create an addition function that accepts any number of parameters
+- Function should return the sum of the parameters
+- If the function encounters the number 10, it should skip it and not add it to the total
+- If the function encounters the number 5, it should subtract it from the total
+"""
+def addition(*nums):
+    total = 0
+    for num in nums:
+        if num == 10:
+            continue
+        elif num == 5:
+            total -= 5
+        else:
+            total += num
+    return total
+# Test cases
+print(addition(10, 20, 30))        # 50
+print(addition(10, 20, 30, 40))    # 100
+print(addition(10, 20, 30, 40, 50))# 150
+print(addition(10, 5, 20, 30))     # 45
+print(addition(10, 5, 20, 30, 5))  # 40
+
+print("--------------------task 2 done -----------------------")
+#---------------------------------------------------------------
