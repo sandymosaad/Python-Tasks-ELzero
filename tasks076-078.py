1df84970383a632294630680a522d37ce23b8135
sandymosaad <sandyzakher5@gmail.com>
2025-10-06 21:14:18 +0300

    tasks076 - 078  Problem 01

---
+++ tasks076-078.py
@@ -0,0 +1,16 @@
+'''
+problem 01:
+Generate random numbers (normal, even, odd) and print module methods
+'''
+import random
+
+print(f'Random Number Between 10 And 50 =>{random.randint(10,50)} ')
+
+print(f'Random Even Number Between 2 And 10 => {random.randrange(2, 11, 2)} ')
+
+print(f"Random Odd Number Between 1 And 9 => {random.randrange(1,10,2)} ")
+
+print("\n# Module Methods Content Here")
+print(dir(random))
+#------------------------task 1 done-------------------------
+#----------------------------------------------------------
