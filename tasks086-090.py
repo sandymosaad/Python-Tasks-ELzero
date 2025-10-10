0a081be2c91fc06cea95db7baa1eec0373cf8dcb
sandymosaad <sandyzakher5@gmail.com>
2025-10-10 15:14:17 +0300

    tasks086 - 090 Problem 02

--- tasks086-090.py
+++ tasks086-090.py
@@ -15,3 +15,23 @@ final_string = ''.join(my_data)
 print(final_string)  # Elzero
 #---------------------task 01 done -------------
 #-----------------------------------------------
+'''
+problem 02:
+Write the correct code inside the loop to get the output "Elzero"
+Do not modify any existing code
+'''
+
+my_list1 = ["E", "L", "Z", "E", "R", "O", 1, 2]
+my_tuple = ("E", "Z", "R", 1, 2, "E", "R", "O")
+my_list2 = ("L", "E", "O", 1, 2, "E", "R", "O")
+my_data = []
+
+for item1, item2, item3 in zip(my_list1, my_tuple, my_list2):
+    # Write Your Code Here
+    if isinstance(item1, str):
+        my_data.append(item1)
+
+final_string = ''.join(my_data)
+print(final_string)  # Elzero
+#-----------------task 2 done -------------
+#------------------------------------------
