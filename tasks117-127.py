85fba76d03c19301a8395a2b138d31eb86b051e3
sandymosaad <sandyzakher5@gmail.com>
2025-10-14 17:17:14 +0300

    tasks 117-127 problem 01:

---
+++ tasks117-127.py
@@ -0,0 +1,12 @@
+'''
+problem 01:
+Write at least 5 different data types that can be used in a database (Database Data Types).
+'''
+# Your Answer Here
+'''
+1. INT          → Used to store integer numbers (e.g., 10, 200, -5)
+2. VARCHAR      → Used to store text with a limited length (e.g., "Sandy")
+3. TEXT         → Used to store long text (e.g., a description or article)
+4. DATE         → Used to store dates (e.g., 2025-10-05)
+5. BOOLEAN      → Used to store logical values (True / False)
+'''
